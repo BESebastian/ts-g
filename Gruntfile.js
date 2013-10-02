34f504df861f2bf3d99b579790060ae5a410b863
Bellend Sebastian <bellend@bellendsebastian.com>
2013-10-02 17:19:55 +0100

    Switching over to grunt.

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function (grunt) {
+    'use strict';
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        typescript: {
+            base: {
+                src: ['src/**/*.ts'],
+                dest: 'public/js/compiled.js',
+                options: {
+                    module: 'amd'
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['src/**/*.ts'],
+                tasks: ['typescript'],
+                options: {
+                    livereload: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-typescript');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['typescript']);
+};
